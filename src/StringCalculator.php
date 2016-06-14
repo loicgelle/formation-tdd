84beb41b740c9549d6b4fe14d4464d19f256eb37
loicgelle <loic.gelle@polytechnique.edu>
2016-06-14 09:47:33 +0200

    Init git

---
+++ src/StringCalculator.php
@@ -0,0 +1,42 @@
+<?php
+class StringCalculator {
+
+    static public function add($num) {
+        if(substr($num, 0, 2) == "//") {
+            $split = explode("\n", $num, 2);
+            $delim = [];
+            preg_match_all("/\[([^\]]*)\]/", $split[0], $delim);
+            $num = $split[1];
+        }
+        else
+            $delim = ["\n"];
+
+        $negativesCaught = [];
+
+        for ($i=0;$i<count($delim);$i++)
+            $num = str_replace($delim[$i], ",", $num);
+        $arrNum = explode(",", $num);
+        $sum = 0;
+
+        for ($i=0;$i<count($arrNum);$i++) {
+            if ($arrNum[$i] >= 0 && $arrNum[$i] <= 1000)
+                $sum += $arrNum[$i];
+            else if ($arrNum[$i] < 0)
+                $negativesCaught[] = $arrNum[$i];
+        }
+
+        $numNeg = count($negativesCaught);
+        if ($numNeg > 0) {
+            $msg = "negatives non allowed: ";
+            for ($i=0;$i<$numNeg;$i++) {
+                $msg = $msg . $negativesCaught[$i];
+                if ($i < $numNeg - 1)
+                    $msg = $msg . ", ";
+            }
+            throw new Exception($msg);
+        } else
+            return $sum;
+    }
+
+}
+    ?>
---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,55 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class StringCalculatorTest extends TestCase {
+
+    public function testAddZero() {
+        $testStr = "";
+        $this->assertEquals(0, StringCalculator::add($testStr));
+    }
+
+    public function testAddOne() {
+        $testStr = "2";
+        $this->assertEquals(2, StringCalculator::add($testStr));
+    }
+
+    public function testAddTwo() {
+        $testStr = "2,4";
+        $this->assertEquals(6, StringCalculator::add($testStr));
+    }
+
+    public function testMultipleDelimiters() {
+        $testStr = "2,4\n3";
+        $this->assertEquals(9, StringCalculator::add($testStr));
+    }
+
+    public function testNegativeHandling() {
+        $testStr = "3,-1,2,-3";
+        $exceptionCaught = false;
+        try {
+            StringCalculator::add($testStr);
+        }
+        catch (Exception $e) {
+            $exceptionCaught = true;
+            $this->assertEquals("negatives non allowed: -1, -3", $e->getMessage());
+        }
+        $this->assertEquals(true, $exceptionCaught);
+    }
+
+    public function testBigNumbers() {
+        $testStr = "3,1002,5";
+        $this->assertEquals(8, StringCalculator::add($testStr));
+    }
+
+    public function testAheadDelimiter() {
+        $testStr = "//[***]\n1***2***3";
+        $this->assertEquals(6, StringCalculator::add($testStr));
+    }
+
+    public function testAheadMultipleDelimiters() {
+        $testStr = "//[**][%]\n1**2%3";
+        $this->assertEquals(6, StringCalculator::add($testStr));
+    }
+
+}
+?>
